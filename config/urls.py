4d2ef63b9af4f3174f1627180978ac222cb2beb6
pleitemarques <p22dro@gmail.com>
2025-06-09 04:29:52 -0300

    update .gitignore and clean up unused admin route

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -7,7 +6,6 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
